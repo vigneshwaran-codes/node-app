94d8c3181699f0ca0e416d70ecb76727e8d5252c
vigneshwarank00 <smilevicky2000@gmail.com>
2022-02-16 00:13:58 +0530

    added node modules in gitignore

--- main.js
+++ main.js
@@ -4,41 +4,49 @@
 
 // console.log('max number is:', Math.max(...arr))
 
-const express = require('express')
-const { MongoClient } = require('MongoDB')
+/* Older Syntax */
+// const express = require('express')
+// const { MongoClient } = require('MongoDB')
+
+/* New syntax */
+import express from 'express'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const app = express()
-const PORT = 5000
-const poll = [
-  {
-    id: '1',
-    company: 'OnePlus',
-    color: 'red',
-    lineSpacing: '4px',
-    content: 'never settle'
-  },
-  {
-    id: '2',
-    company: 'Apple',
-    color: 'crimson',
-    lineSpacing: '3px',
-    content: 'Think Different ,not too Different'
-  },
-  {
-    id: '3',
-    company: 'samsung',
-    color: 'skyblue',
-    content: 'innovation of Galaxy'
-  },
-  {
-    id: '4',
-    company: 'Mi',
-    color: 'orange',
-    content: 'Just for fans'
-  }
-]
+const PORT = process.env.PORT
+// const poll = [
+//   {
+//     id: '1',
+//     company: 'OnePlus',
+//     color: 'red',
+//     lineSpacing: '4px',
+//     content: 'never settle'
+//   },
+//   {
+//     id: '2',
+//     company: 'Apple',
+//     color: 'crimson',
+//     lineSpacing: '3px',
+//     content: 'Think Different ,not too Different'
+//   },
+//   {
+//     id: '3',
+//     company: 'samsung',
+//     color: 'skyblue',
+//     content: 'innovation of Galaxy'
+//   },
+//   {
+//     id: '4',
+//     company: 'Mi',
+//     color: 'orange',
+//     content: 'Just for fans'
+//   }
+// ]
 async function createConnection () {
-  const MONGO_URL =
-    'mongodb+srv://vigneshwarank:aEvL6GhZAIhddFVC@cluster0.p5wxf.mongodb.net/contestants?retryWrites=true&w=majority'
+  const MONGO_URL = process.env.MONGO_URI
   // Todo
   const client = new MongoClient(MONGO_URL)
 
@@ -59,6 +67,15 @@ async function getPollById (client, id) {
   console.log('Succesfully Connected', result)
   return result
 }
+async function getPolls (client, filter) {
+  const result = await client
+    .db('contestants')
+    .collection('poll')
+    .find(filter)
+    .toArray()
+  console.log('Succesfully Connected', result)
+  return result
+}
 
 async function insertPoll (client, poll) {
   const result = await client
@@ -74,8 +91,31 @@ app.get('/', (request, response) => {
   response.send('Hello Vigneshwaran K')
 })
 
-app.get('/poll', (request, response) => {
-  response.send(poll)
+app.get('/poll', async (request, response) => {
+  const client = await createConnection()
+  const contestantAll = await getPolls(client, { id: { $gt: 1 } })
+  response.send(contestantAll)
+})
+
+app.get('/poll/name/:companyname', async (request, response) => {
+  const companyname = request.params.companyname
+  const client = await createConnection()
+  const contestants = await getPolls(client, { company: companyname })
+  response.send(contestants)
+})
+
+app.get('/poll/color/:colorname', async (request, response) => {
+  const colorname = request.params.colorname
+  const client = await createConnection()
+  const contestants = await getPolls(client, { color: colorname })
+  response.send(contestants)
+})
+
+app.get('/poll/content/:description', async (request, response) => {
+  const description = request.params.description
+  const client = await createConnection()
+  const contestants = await getPolls(client, { content: { $regex: description, $options: 'i' } })
+  response.send(contestants)
 })
 
 app.get('/poll/:id', async (request, response) => {
